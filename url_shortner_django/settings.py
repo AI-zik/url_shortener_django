a4fc506d0cbfad129514dcc3ab1906347092ac09
Isaac Adejumo <isaacadejumo01@gmail.com>
2022-07-05 15:06:11 +0100

    fixed allowed hosts

--- url_shortner_django/settings.py
+++ url_shortner_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = os.environ.get('DEBUG') == 'True'
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ai-zik-url-shortener.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
